d9693c780eae4da9a23445fa895612aed05424f3
YangLang <1004145468@qq.com>
2017-03-31 16:00:38 +0800

    添加上拉加载更多

--- Player/app/src/main/java/com/ruo/player/views/LineProgressView.java
+++ Player/app/src/main/java/com/ruo/player/views/LineProgressView.java
@@ -1,6 +1,7 @@
 package com.ruo.player.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -22,4 +23,13 @@ public class LineProgressView extends View{
         super(context, attrs, defStyleAttr);
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
