0b9b3454a96347bb873c565ab84d2d4d16fcc027
YangLang <1004145468@qq.com>
2017-04-01 14:27:11 +0800

    解决oppo悬浮框禁止导致应用崩溃bug

--- Player/app/src/main/java/com/ruo/player/service/WindowService.java
+++ Player/app/src/main/java/com/ruo/player/service/WindowService.java
@@ -5,7 +5,9 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
+import com.ruo.player.Utils.DialogUtils;
 import com.ruo.player.Utils.PLGT;
 import com.ruo.player.Utils.WindowUtils;
 import com.ruo.player.views.WindowPlayerView;
@@ -50,6 +52,13 @@ public class WindowService extends Service {
                 WindowUtils.refreshScreenViewPosition(WindowService.this, windowPlayerView, e.getRawX(), e.getRawY());
             }
         });
-        WindowUtils.addScreenView(this, windowPlayerView);
+        //解决 oppo 手机没有授权，应用崩溃
+        try {
+            WindowUtils.addScreenView(this, windowPlayerView);
+        } catch (Exception e) {
+            e.printStackTrace();
+            DialogUtils.showToast(this, "悬浮窗权限禁止");
+            stopSelf();
+        }
     }
 }
